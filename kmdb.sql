74b37edadd0e572643880b171913e65b5e57d6fe
alfredosordo <alfredo.sordo@kellogg.northwestern.edu>
2022-04-22 00:37:29 +0000

    hw_v3

--- kmdb.sql
+++ kmdb.sql
@@ -133,6 +133,28 @@ VALUES
 ('The Dark Knight Rises', '2012', 'PG-13', 'Warner Bros.');
 
 
+INSERT INTO casts (
+movie,
+actor,
+character_played
+)
+VALUES
+('Batman Begins', 'Christian Bale', 'Bruce Wayne'),
+('Batman Begins', 'Michael Caine', 'Alfred'),
+('Batman Begins', 'Liam Neeson', "Ra's Al Ghul"),
+('Batman Begins', 'Katie Holmes', 'Rachel Dawes'),
+('Batman Begins', 'Gary Oldman', 'Commissioner Gordon'),
+('The Dark Knight', 'Christian Bale', 'Bruce Wayne'),
+('The Dark Knight', 'Heath Ledger', 'Joker'),
+('The Dark Knight', 'Aaron Eckhart', 'Harvey Dent'),
+('The Dark Knight', 'Michael Caine', 'Alfred'),
+('The Dark Knight', 'Maggie Gyllenhaal', 'Rachel Dawes'),
+('The Dark Knight Rises', 'Christian Bale', 'Bruce Wayne'),
+('The Dark Knight Rises', 'Gary Oldman', 'Commissioner Gordon'),
+('The Dark Knight Rises', 'Tom Hardy', 'Bane'),
+('The Dark Knight Rises', 'Joseph Gordon-Levitt', 'John Blake'),
+('The Dark Knight Rises', 'Anne Hathaway', 'Selina Kyle');
+
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -150,4 +172,4 @@ select * from movies;
 
 
 -- The SQL statement for the cast output
--- TODO!
+select * from casts;
